d1bcfa019f0d1c80082eff6a478b9e0512dc79dc
Van Pho Nguyen <vanpho02@gmail.com>
2017-08-23 20:54:41 +0700

    xong bai 2

--- b.js
+++ b.js
@@ -6,6 +6,19 @@
 ****
 *****
 
+    *
+   **
+  ***
+ ****
+*****
+
+i la so Dong
+j la so cot
+1 - 5
+2 - 4
+3 - 2 
+
+i + j >= soDong + 1
 */
 
 function veHinh1(soDong) {
@@ -19,7 +32,26 @@ function veHinh1(soDong) {
 }
 
 function veHinh2(soDong) {
-    for(let s = ''; s.length <= soDong; s += '*') console.log(s);
+    for(let i = 1; i <= soDong; i++ ) {
+        let s = '';
+        for(let j = 1; j <= soDong; j++ ) {
+            s += (i + j >= soDong + 1) ? '*' : ' ';
+        }
+        console.log(s);
+    }
 }
 
 veHinh2(5);
+
+/*
+    *
+   ***
+  *****
+ *******
+
+ soDong = 4 
+*/
+
+function veHinh3(soDong) {
+    
+}
