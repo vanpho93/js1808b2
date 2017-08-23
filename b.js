889386dc510dd49ae9e5527b6115723bcad58f1d
Van Pho Nguyen <vanpho02@gmail.com>
2017-08-23 21:09:58 +0700

    veHinh3

--- b.js
+++ b.js
@@ -41,14 +41,19 @@ function veHinh2(soDong) {
     }
 }
 
-veHinh2(5);
+// veHinh2(5);
 
 /*
     *
    ***
   *****
  *******
+dong 1 -> khoang cach <= 0
+dong 2 -> khoang cach <= 1
+dong 3 -> khoang cach <= 2
+dong 4 -> khoang cach <= 3
 
+|j - soDong| <= i - 1
  soDong = 4 
 */
 
@@ -64,5 +69,13 @@ veHinh2(5);
 */
 
 function veHinh3(soDong) {
-
+    for(let i = 1; i <= soDong; i++ ) {
+        let s = '';
+        const soCot = soDong * 2 - 1;
+        for(let j = 1; j <= soCot; j++ ) {
+            const dieuKien = Math.abs(j - soDong) <= i - 1;
+            s += dieuKien ? '*' : ' ';
+        }
+        console.log(s);
+    }
 }
